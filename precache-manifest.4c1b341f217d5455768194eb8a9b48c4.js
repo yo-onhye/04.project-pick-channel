05765c733ba7d00e9e7923c200616ebea246f3e4
yo-onhye <woony0807@gmail.com>
2020-07-07 12:19:16 +0900

    Updates

--- precache-manifest.4c1b341f217d5455768194eb8a9b48c4.js
+++ precache-manifest.4c1b341f217d5455768194eb8a9b48c4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b2971c719924029beb77c8cba19779e6",
+    "revision": "2a19a419a057d7c674cbde13263f00a7",
     "url": "/index.html"
   },
   {
-    "revision": "58266e4905b68e546392",
+    "revision": "3ee98da30d4bbc66343d",
     "url": "/static/css/main.40e644ab.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.85b6ae81.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58266e4905b68e546392",
-    "url": "/static/js/main.3b640b41.chunk.js"
+    "revision": "3ee98da30d4bbc66343d",
+    "url": "/static/js/main.1de8dc8d.chunk.js"
   },
   {
     "revision": "9ccf36a52585c82b0267",
